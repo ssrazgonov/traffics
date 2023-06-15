10bb8eff881324b0ec21169b687bc81691e1328a
ssrazgonov <ssrazgonov@gmail.com>
2023-06-15 23:52:43 +0300

    fix

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -15,23 +15,17 @@
     <p class="mt-2">Не обработанные заявки: {{$appealsNotProcessed}} из {{$appeals}}</p>
     <div class="progress mb-3">
 
-        <div class="progress-bar progress-bar-striped bg-warning" role="progressbar" style="width: {{$appealsNotProcessedProc}}%" aria-valuenow="{{$appealsNotProcessedProc}}" aria-valuemin="0" aria-valuemax="100">
-            {{$appealsNotProcessedProc}}%
-        </div>
+        <div class="progress-bar progress-bar-striped bg-warning" role="progressbar" style="width: {{$appealsNotProcessedProc}}%" aria-valuenow="{{$appealsNotProcessedProc}}" aria-valuemin="0" aria-valuemax="100"></div>
     </div>
 
     <p class="mt-2">Заявки в работе: {{$appealsInWork}} из {{$appeals}}</p>
     <div class="progress mb-3">
-        <div class="progress-bar progress-bar-striped bg-info" role="progressbar" style="width: {{$appealsInWorkProc}}%" aria-valuenow="{{$appealsInWorkProc}}" aria-valuemin="0" aria-valuemax="100">
-            {{$appealsInWorkProc}}%
-        </div>
+        <div class="progress-bar progress-bar-striped bg-info" role="progressbar" style="width: {{$appealsInWorkProc}}%" aria-valuenow="{{$appealsInWorkProc}}" aria-valuemin="0" aria-valuemax="100"></div>
     </div>
 
     <p class="mt-2">Выполненные заявки: {{$appealsComplete}} из {{$appeals}}</p>
     <div class="progress mb-3">
-        <div class="progress-bar progress-bar-striped bg-success" role="progressbar" style="width: {{$appealsCompleteProc}}%" aria-valuenow="{{$appealsCompleteProc}}" aria-valuemin="0" aria-valuemax="100">
-            {{$appealsCompleteProc}}%
-        </div>
+        <div class="progress-bar progress-bar-striped bg-success" role="progressbar" style="width: {{$appealsCompleteProc}}%" aria-valuenow="{{$appealsCompleteProc}}" aria-valuemin="0" aria-valuemax="100"></div>
     </div>
 
     <h3 class="mt-5">Светофоры</h3>
@@ -39,15 +33,11 @@
     <p class="mt-2">В рабочем состоянии: {{$tfW}} из {{$tf}}</p>
     <div class="progress mb-3">
 
-        <div class="progress-bar progress-bar-striped bg-success" role="progressbar" style="width: {{$tfWProc}}%" aria-valuenow="{{$tfWProc}}" aria-valuemin="0" aria-valuemax="100">
-            {{$tfWProc}}%
-        </div>
+        <div class="progress-bar progress-bar-striped bg-success" role="progressbar" style="width: {{$tfWProc}}%" aria-valuenow="{{$tfWProc}}" aria-valuemin="0" aria-valuemax="100"></div>
     </div>
 
     <p class="mt-2">В ремонте: {{$tfNW}} из {{$tf}}</p>
     <div class="progress mb-3">
-        <div class="progress-bar progress-bar-striped bg-danger" role="progressbar" style="width: {{$tfNWProc}}%" aria-valuenow="{{$tfNWProc}}" aria-valuemin="0" aria-valuemax="100">
-            {{$tfNWProc}}%
-        </div>
+        <div class="progress-bar progress-bar-striped bg-danger" role="progressbar" style="width: {{$tfNWProc}}%" aria-valuenow="{{$tfNWProc}}" aria-valuemin="0" aria-valuemax="100"></div>
     </div>
 @endsection
